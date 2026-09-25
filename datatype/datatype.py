5b9754486fdbd95f6aaaf9b217372c1cc6395daf
zuyairiyaakternafisa1 <zuyairiyaakternafisa1@gmail..com>
2026-09-26 00:32:48 +0600

    folder create  and data type create

---
+++ datatype/datatype.py
@@ -0,0 +1,23 @@
+age = 30;
+
+
+price = 300.500
+
+
+
+ishot = True
+
+print(type (age))
+
+
+print(type(price))
+
+
+print(type(ishot))
+
+
+
+name = "abdullah al kawser"
+
+
+print(type(name))
---
+++ variable/value.py
@@ -0,0 +1,2 @@
+name = "kawser"
+print(name)
