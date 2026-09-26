7db1dc59f8cf111763c1fcfaa447278e176871b3
zuyairiyaakternafisa1 <zuyairiyaakternafisa1@gmail..com>
2026-09-26 22:54:33 +0600

    print

---
+++ python besic/demo.py
@@ -0,0 +1,10 @@
+name = "Nafishaaaaaaa"
+
+age = 20;
+
+department ="software enginnering"
+
+print("name ",name)
+print("age ",age)
+print("department ",department)
+
---
+++ python besic/program.py/a.py
@@ -0,0 +1,13 @@
+name = "Zuyairiya"
+age = 20
+student_id = 101
+department = "Computer Science"
+city = "Dhaka"
+
+print("Student Information")
+print("-------------------")
+print("Name:", name)
+print("Age:", age)
+print("Student ID:", student_id)
+print("Department:", department)
+print("City:", city)
